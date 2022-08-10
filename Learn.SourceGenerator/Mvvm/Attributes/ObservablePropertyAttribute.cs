aae22163d66b1c8246f26f40b888248fce004b71
Damian Suess <djsuess@gmail.com>
2022-08-10 13:03:47 -0400

    ObservableProperty

---
+++ Learn.SourceGenerator/Mvvm/Attributes/ObservablePropertyAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.CodeAnalysis;
+
+namespace Learn.SourceGenerator.Mvvm.Attributes
+{
+  /// <summary>
+  ///   Attribute for generated property containing type of <see cref="ObservableObject"/>.
+  ///   <para>
+  ///     <code>
+  ///     public class SomeViewModel
+  ///     {
+  ///       [ObservableProperty]
+  ///       private string _isEnabled;
+  ///     }
+  ///     </code>
+  ///   </para>
+  /// </summary>
+  /// <remarks>
+  ///   The generated propty should be able to recognize fields using the following formats
+  ///   <c>pascalCase</c> or <c>_pascalCase</c>. The resulting converstion will generate
+  ///   a property which is in CamelCase.
+  /// </remarks>
+  [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+  public sealed class ObservablePropertyAttribute : Attribute
+  {
+  }
+}
--- Learn.SourceGenerator/Mvvm/ObservablePropertyGenerator.cs
+++ Learn.SourceGenerator/Mvvm/ObservablePropertyGenerator.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Learn.SourceGenerator.Mvvm
 {
-  public sealed partial class ObservablePropertyGenerator
+  /// <summary>
+  ///   Source code generator for <c>ObservablePropertyAttribute</c>.
+  /// </summary>
+  /// <remarks>
+  ///   REF: https://github.com/CommunityToolkit/dotnet/blob/main/CommunityToolkit.Mvvm.SourceGenerators/ComponentModel/ObservablePropertyGenerator.cs
+  /// </remarks>
+  public sealed partial class ObservablePropertyGenerator : IIncrementalGenerator
   {
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+      IncrementalValuesProvider<IFieldSymbol> fieldSymbols =
+          context.SyntaxProvider
+          .CreateSyntaxProvider(
+              static (node, _) => node is FieldDeclarationSyntax { Parent: ClassDeclarationSyntax or RecordDeclarationSyntax, AttributeLists.Count: > 0 },
+              static (context, _) => ((FieldDeclarationSyntax)context.Node).Declaration.Variables.Select(v => (IFieldSymbol)context.SemanticModel.GetDeclaredSymbol(v)!))
+          .SelectMany(static (item, _) => item);
+
+    }
   }
 }
